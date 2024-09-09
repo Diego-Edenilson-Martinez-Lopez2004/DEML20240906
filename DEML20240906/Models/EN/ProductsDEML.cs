7230825b4f85837d803703ae7d99b9736e96b329
Diego-Edenilson-Martinez-Lopez2004 <diego.edenilson.martinez.lopez.n25@gmail.com>
2024-09-08 22:49:25 -0600

    se creo la vista delete y se modifico el _Layout

--- DEML20240906/Models/EN/ProductsDEML.cs
+++ DEML20240906/Models/EN/ProductsDEML.cs
@@ -1,8 +1,11 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace DEML20240906.Models.EN
 {
     public class ProductsDEML
     {
+        [Required(ErrorMessage = "El campo Id es obligatorio.")]
         public int Id { get; set; }
 
         public string NombreDEML { get; set; }
